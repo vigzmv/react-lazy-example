f21fb84dae17cd1099233f973453b0407d1919f3
vigzmv <vigzmv@outlook.com>
2018-10-24 18:36:13 +0530

    feat(lazy): add react lazy

--- src/App.js
+++ src/App.js
@@ -1,22 +1,39 @@
 import React, { Component } from 'react';
 import './App.css';
-import Header from './Header';
+const User = React.lazy(() => import('./User'));
+
+const fetchRandomUser = () =>
+  fetch('https://randomuser.me/api/')
+    .then(res => res.json())
+    .then(res => res.results[0]);
+
 class App extends Component {
   state = {
-    showLogo: false,
+    user: null,
   };
 
-  showLogo = () => {
-    this.setState({
-      showLogo:true
-    })
-  }
+  loadUser = () => {
+    this.setState(
+      {
+        loading: true,
+      },
+      async () => this.setState({ user: await fetchRandomUser(), loading: false }),
+    );
+  };
+
   render() {
-    const { showLogo } = this.state;
+    const { user, loading } = this.state;
 
     return (
       <div className="App">
-        {showLogo ? <Header /> : <button onClick={this.showLogo}>Press Me!!</button>}
+        <React.Suspense fallback={<div>Loading Component...</div>}>
+          {user && <User user={user}/>}
+          {loading ? (
+            <div>Loading User...</div>
+          ) : (
+            !user && <button onClick={this.loadUser}>Press Me!!</button>
+          )}
+        </React.Suspense>
       </div>
     );
   }
