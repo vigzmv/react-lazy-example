0cebfcb7a47c0ce707bf6053cc4ce34941db5448
vigzmv <vigzmv@outlook.com>
2018-10-24 19:21:29 +0530

    Updates

---
+++ precache-manifest.7a264e5e5b37f6d1903fd5f446c27e9c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a41272f3ba5e5bb26d93",
+    "url": "/react-lazy-example/static/js/runtime~main.a41272f3.js"
+  },
+  {
+    "revision": "5db567f44318a1009e1e",
+    "url": "/react-lazy-example/static/js/main.5db567f4.chunk.js"
+  },
+  {
+    "revision": "6ec5b4724cee78be4a8a",
+    "url": "/react-lazy-example/static/js/2.6ec5b472.chunk.js"
+  },
+  {
+    "revision": "03f0c6a2d2fefb3c85a6",
+    "url": "/react-lazy-example/static/js/1.03f0c6a2.chunk.js"
+  },
+  {
+    "revision": "5db567f44318a1009e1e",
+    "url": "/react-lazy-example/static/css/main.eb224f39.chunk.css"
+  },
+  {
+    "revision": "5f799348cec109363894d2257050508e",
+    "url": "/react-lazy-example/index.html"
+  }
+];
