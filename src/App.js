8f2452414664896f86ede7922ee2675ac891d1e8
vigzmv <vigzmv@outlook.com>
2018-10-24 18:09:33 +0530

    feat(base)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import Header from './Header';
 class App extends Component {
+  state = {
+    showLogo: false,
+  };
+
+  showLogo = () => {
+    this.setState({
+      showLogo:true
+    })
+  }
   render() {
+    const { showLogo } = this.state;
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {showLogo ? <Header /> : <button onClick={this.showLogo}>Press Me!!</button>}
       </div>
     );
   }
